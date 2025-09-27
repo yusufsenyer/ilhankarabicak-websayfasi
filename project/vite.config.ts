8e4ba2a08af804b83991ab5f6079843d074853e0
yusufsenyer <yusuf555senyer@gmail.com>
2025-09-27 19:13:18 +0300

    Fix vite config root path and outDir paths

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ilhankarabicak-websayfasi/',
+  root: path.resolve(__dirname, '.'),
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(__dirname, 'dist'),
     assetsDir: 'assets',
     sourcemap: false,
     minify: true,
